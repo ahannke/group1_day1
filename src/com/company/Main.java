6ee65a4f3591854639c8eda14a039a126512e3c3
Anna <ahannke@gmail.com>
2016-07-25 11:56:47 +0200

    variable defined

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,8 +4,10 @@ public class Main {
 
     public static void main(String[] args) {
 
+        int result = 2 + 2;
+
         System.out.println("Hi");
-        System.out.println("Yo man!");
+        System.out.println("Your result is : " +result );
         // write your code here
     }
 }
